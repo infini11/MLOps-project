991e8094e37ef5b8299adfc0715b940b083c0bf3
Sakhite MBOUP <130978479+Sakhite@users.noreply.github.com>
2023-10-27 17:04:15 +0200

    data imputation and data transformation

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -43,11 +43,15 @@ def group_by_feature_by_date(df_feature: pd.DataFrame) -> pd.DataFrame:
     Returns:
         pd.DataFrame: data aggregated
     """
-    data_date = df_feature.groupby("Date").agg({"Temperature":"mean"
-                                                ,"Fuel_Price":"mean"
-                                                ,"IsHoliday":"sum"
-                                                ,"CPI":"mean"
-                                                ,"Unemployment":"mean"})
+    data_date = df_feature.groupby("Date").agg(
+        {
+            "Temperature":"mean", 
+            "Fuel_Price":"mean",
+            "IsHoliday":"sum",
+            "CPI":"mean",
+            "Unemployment":"mean"
+        }
+    )
     data_date = data_date.sort_index()
     temp_date_data = data_date[:'2012-12-10']
 
@@ -87,7 +91,10 @@ def merge_feature_and_sales(df_feature: pd.DataFrame, df_sales: pd.DataFrame) ->
     return df_sales_features
 
 
-def agg_store_on_temp_fuel_price_holiday(df_store: pd.DataFrame, df_feature: pd.DataFrame, df_sales: pd.DataFrame) -> pd.DataFrame:
+def agg_store_on_temp_fuel_price_holiday(df_store: pd.DataFrame, 
+                                         df_feature: pd.DataFrame, 
+                                         df_sales: pd.DataFrame
+                                        ) -> pd.DataFrame:
     """scall columns (temperature, fuel price) in df_store by mean, (weekly_sales and isholliday by sum)
 
     Args:
@@ -117,14 +124,101 @@ def agg_store_on_temp_fuel_price_holiday(df_store: pd.DataFrame, df_feature: pd.
 
     return df_store
 
-def transform_data(df: pd.DataFrame) -> None:
-    pass
+def dataset_construction(df_sales: pd.DataFrame,
+                          df_feature: pd.DataFrame,
+                          df_store: pd.DataFrame
+                        ) -> pd.DataFrame:
+    """create dataset and divide the dataset into train and test data
 
-def split_data(df: pd.DataFrame) -> None:
-    pass
+    Args:
+        df_sales (pd.DataFrame): sales data
+        df_features (pd.DataFrame): features data
+        df_store (pd.DataFrame): stores data
+
+    Returns:
+        pd.DataFrame : dataset as dataframe
+    """
+
+    sales_date_store = df_sales.groupby(["Date","Store"]).agg({"Weekly_Sales":"sum"})
+    sales_date_store.sort_index(inplace=True)
+    sales_date_store.Weekly_Sales = sales_date_store.Weekly_Sales/10000
+    sales_date_store.Weekly_Sales = sales_date_store.Weekly_Sales.apply(int)
+    data_table = pd.merge(df_feature, sales_date_store,  how='left', on=["Date", "Store"])
+    data_table = pd.merge(data_table, df_store[["Store", "Type"]],  how='left', on=["Store"])
+
+    return data_table
+
+
+def markdown_data_imputation(data_table: pd.DataFrame, col_to_impute: list) -> pd.DataFrame:
+    """impute missing values
+
+    Args:
+        data_table (pd.DataFrame): dataset
+        col_to_impute (list): list of column to impute
+
+    Returns:
+        pd.DataFrame: dataset imputed
+    """
+    itt = IterativeImputer()
+    df = itt.fit_transform(data_table[col_to_impute])
+    compte = 0
+    for col in col_to_impute:
+        data_table[col] = df[:,compte]
+        compte = compte + 1
+    
+    return data_table
+
+
+def data_imputation_by_mean(data_table: pd.DataFrame, cols: list) -> pd.DataFrame:
+    """impute data by mean
+
+    Args:
+        data_table (pd.DataFrame): dataset
+        cols (list): col to impute by mean
+
+    Returns:
+        pd.DataFrame: data imputed by mean
+    """
+    CPI = cols[0]
+    Unemployment = cols[1]
+    data_table[CPI].fillna((data_table[CPI].mean()), inplace=True)
+    data_table[Unemployment].fillna((data_table[Unemployment].mean()), inplace=True)
+
+    return data_table
+
+def createdummies(data, cols):
+    for col in cols:
+        one_hot = pd.get_dummies(data[col], prefix=col)
+        data = data.join(one_hot)
+        data.drop(col, axis = 1, inplace=True)
+
+    return data
+
+
+def create_columns_and_convert_categorical_data(data_table: pd.DataFrame) -> pd.DataFrame:
+    """create columns and convert categorical data
+
+    Args:
+        data_table (pd.DataFrame): dataset
+
+    Returns:
+        pd.DataFrame: transformed data
+    """
+    data_table['IsHoliday'] = data_table['IsHoliday'].map({True:0,False:1})
+    data_table["Month"] = data_table.Date.dt.month
+    data_table["Year"] = data_table.Date.dt.year
+    data_table["WeekofYear"] = data_table.Date.dt.weekofyear
+    data_table.drop(['Date'], axis=1, inplace=True)
+
+    #create dummies out of categorical column
+    data_table = createdummies(data_table, ["Type", "Month", "Year", "WeekofYear"])
+    
+    return data_table
+
 
 def data_processing_with_io(df: pd.DataFrame) -> None:
     pass
+
 
 def data_processing(df: pd.DataFrame) -> None:
     pass
