8dafe488011793c899bc097252fd228197079992
Sakhite MBOUP <130978479+Sakhite@users.noreply.github.com>
2023-10-31 16:36:54 +0100

    first version of traing function done training dag complete

--- dags/train.py
+++ dags/train.py
@@ -4,7 +4,7 @@ from airflow.decorators import dag, task
 sys.path.append('.')
 print(sys.path)
 from src.preprocessing import data_processing_with_io
-from src.train_model import *
+from src.train_model import train_model_io
 
 from config.config import (DATA_FOLDER, ML_DATASET_OUTPUT_FOLDER, NAME_FILES, TRAIN_DATA, TEST_DATA, START_DATE,
                          CONCURRENCY, SCHEDULE_INTERVAL, DEFAULT_ARGS, TRACKING_URI, COL_TO_IMPUTE, COLS_IMPUTE_BY_MEAN,
@@ -31,9 +31,19 @@ def training_dag():
                                 output_path=output_path)
 
 
-    # @task
-    # def training_model():
-    #     pass
+    @task
+    def training_model_task(tracking_uri: str,
+                            models_params: dict,
+                            train_path : str,
+                            test_path: str,
+                            output_path: str) -> None:
+
+        train_model_io(tracking_uri=tracking_uri,
+                       models_params=models_params,
+                       train_path=train_path,
+                       test_path=test_path,
+                       output_path=output_path)
+        
 
 
     preprocessing_data_task(base_path=DATA_FOLDER,
@@ -42,7 +52,11 @@ def training_dag():
                        cols_=COLS_IMPUTE_BY_MEAN,
                        output_path=ML_DATASET_OUTPUT_FOLDER)
     
-    # training_model()
+    training_model_task(tracking_uri=TRACKING_URI,
+                        models_params=MODELS_PARAM,
+                        train_path=TRAIN_DATA,
+                        test_path=TEST_DATA,
+                        output_path=ML_DATASET_OUTPUT_FOLDER)
 
 
 train_ml_dag = training_dag()
--- src/train_model.py
+++ src/train_model.py
@@ -1,9 +1,10 @@
 import sys
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import accuracy_score
+import mlflow
 
 sys.path.append('.')
 from src.preprocessing import *
@@ -42,12 +43,86 @@ def split_data_X_y_train_test(df_train: pd.DataFrame, testsize: float) -> Tuple[
     return (X_train, X_test, y_train, y_test)
 
 
-def train(models_params: dict, data : list):
-    pass
+def train(tracking_uri: str, models_params: dict, data : list) -> pd.DataFrame:
+    """train selectioned model and store result in some dataframe
 
-def train_model_io():
-    pass
+    Args:
+        models_params (dict): dict of model params
+        data (list): train and test data
+
+    Returns:
+        pd.DataFrame: a dataframe with model and score sorted
+    """
+
+    mlflow.set_tracking_uri(tracking_uri)
+
+    with mlflow.start_run():
+
+        X_train, X_test, y_train, y_test = data
+
+        name = []
+        score = []
+        models = []
+        rmse = []
+
+        for model_param in models_params:
+
+            grid_search = GridSearchCV(estimator=model_param['model'],
+                                    param_grid=model_param['grid_parameters'],
+                                    scoring='rmse',
+                                    cv=5,
+                                    verbose=5,
+                                    n_jobs=-1
+                                )
+            
+            grid_search.fit(X_train, y_train)
+            name.append(type(grid_search).__name__)
+            score.append(grid_search.score(X_test, y_test))
+            
+            models.append(grid_search)
+            rmse.append(np.sqrt(mean_squared_error(grid_search.predict(X_test), y_test)))
+
+            mlflow.log_param('best_param', grid_search.best_params_)
+            mlflow.log_param('rmse', np.sqrt(mean_squared_error(grid_search.predict(X_test), y_test)))
+            mlflow.log_param('score', grid_search.score(X_test, y_test))
+
+        df_score = pd.DataFrame(
+            list(zip(name, rmse, score, models)),
+            columns=['name', 'rmse', 'score', "model"]
+        )
+        df_score.set_index('name', inplace=True)
+        df_score.sort_values(by=['score'], inplace=True)
 
 
-def train_model():
-    pass
+    return df_score
+
+def train_model_io(tracking_uri: str, models_params: dict, train_path : str, test_path, output_path: str):
+    
+    df_score, best_model_row = train_model(tracking_uri=tracking_uri,
+                           models_params=models_params,
+                           train_path=train_path,
+                           test_path=test_path
+                        )
+
+    df_score.to_csv(os.path.join(output_path, 'df_model_score.csv'), index=False)
+    best_model_row.to_csv(os.path.join(output_path, 'best_row_model.csv'), index=False)
+
+
+def train_model(tracking_uri: str, models_params: dict, train_path : str, test_path):
+
+    df_train, df_test = load_train_and_test_data(
+        train_path=train_path,
+        test_path=test_path
+    )
+
+    X_train, X_test, y_train, y_test = split_data_X_y_train_test(df_train=df_train, testsize=0.2)
+
+    df_score = train(tracking_uri=tracking_uri, models_params=models_params, data=[X_train, X_test, y_train, y_test])
+
+    best_model_row = df_score.iloc[-1]
+    # y_true = df_test['Weekly_Sales']
+    # df_test.drop(['Weekly_Sales'], axis=1, inplace=True)
+    # pred = best_model_row['model'].predict(df_test)
+
+    return df_score, best_model_row
+
